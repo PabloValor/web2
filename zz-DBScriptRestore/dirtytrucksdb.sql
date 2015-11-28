239918ad466f0d7c98a140508c61d0b64fd54025
Pablo Valor <pablovalor89@gmail.com>
2015-11-28 19:11:47 -0300

    Se actualiza BD

--- zz-DBScriptRestore/dirtytrucksdb.sql
+++ zz-DBScriptRestore/dirtytrucksdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2015 at 04:22 PM
+-- Generation Time: Nov 28, 2015 at 11:11 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -66,7 +66,7 @@ INSERT INTO `cargo` (`ID`, `DESCRIPCION`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `ID` int(3) NOT NULL,
+  `ID` int(11) NOT NULL,
   `RAZON_SOCIAL` varchar(25) NOT NULL,
   `ACTIVO` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -94,6 +94,13 @@ CREATE TABLE IF NOT EXISTS `destino` (
   `ID_PROV` int(11) DEFAULT NULL,
   `ID_PAIS` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `destino`
+--
+
+INSERT INTO `destino` (`ID`, `DIRECCION`, `NUMERO`, `ID_LOCALIDAD`, `ID_PROV`, `ID_PAIS`) VALUES
+(1, 'Sarrachaga', 103, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -139,7 +146,7 @@ INSERT INTO `empleado` (`ID`, `NOMBRE`, `APELLIDO`, `DNI`, `SEXO`, `FECHA_NACIMI
 (40, 'Test', 'Test', 0, 'M', '0000-00-00', '0000-00-00', '0.00', 1, '', '', 1, '', 'foto14'),
 (41, 'Test', 'Test', 0, 'M', '0000-00-00', '0000-00-00', '0.00', 1, '', '', 2, '', 'foto15'),
 (43, 'Hola ', 'Test', 0, 'M', '0000-00-00', '0000-00-00', '0.00', 1, '', '', 3, '', 'foto16'),
-(44, 'Test', 'Test', 0, 'M', '0000-00-00', '0000-00-00', '0.00', 2, '', '', 2, '', 'foto1'),
+(44, 'Test', 'Test', 0, 'M', '0000-00-00', '0000-00-00', '0.00', 2, '', '', 2, '', 'foto10'),
 (45, 'German', 'Polosky', 0, 'M', '1993-03-12', '2015-04-11', '0.00', 1, '', '', 1, '', '');
 
 -- --------------------------------------------------------
@@ -152,6 +159,15 @@ CREATE TABLE IF NOT EXISTS `localidad` (
   `ID` int(11) NOT NULL DEFAULT '0',
   `DESCRIPCION` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `localidad`
+--
+
+INSERT INTO `localidad` (`ID`, `DESCRIPCION`) VALUES
+(1, '25 de Mayo'),
+(2, 'Rawson'),
+(3, 'Antofagasta');
 
 -- --------------------------------------------------------
 
@@ -182,7 +198,7 @@ CREATE TABLE IF NOT EXISTS `parada` (
   `ID` int(11) NOT NULL,
   `ID_DESTINO` int(11) DEFAULT NULL,
   `COORX` varchar(50) DEFAULT NULL,
-  `COORDY` varchar(50) DEFAULT NULL
+  `COORY` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -305,7 +321,14 @@ CREATE TABLE IF NOT EXISTS `viaje` (
   `CANT_KILOMETROS` decimal(10,2) NOT NULL,
   `ID_TIPO_ACOPLADO` int(11) NOT NULL,
   `ID_EMPLEADO` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `viaje`
+--
+
+INSERT INTO `viaje` (`ID`, `DOMINIO_VEHICULO`, `ID_DESTINO`, `ID_CLIENTE`, `FECHA_PROGRAMADA`, `FECHA_INICIO`, `FECHA_FIN`, `CANT_KILOMETROS`, `ID_TIPO_ACOPLADO`, `ID_EMPLEADO`) VALUES
+(2, 'AXE-752', 1, 1, '0000-00-00', '0000-00-00', '0000-00-00', '500.00', 1, 22);
 
 -- --------------------------------------------------------
 
@@ -442,7 +465,7 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT for table `viaje`
 --
 ALTER TABLE `viaje`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
