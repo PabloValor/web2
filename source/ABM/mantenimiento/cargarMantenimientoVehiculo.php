540291b5ed797ba33e941f673859de3302a4cba8
Manuel <h.torres.baquedano@gmail.com>
2015-11-30 23:32:33 -0300

    Se edita formulario de mantenimiento

--- source/ABM/mantenimiento/cargarMantenimientoVehiculo.php
+++ source/ABM/mantenimiento/cargarMantenimientoVehiculo.php
@@ -7,43 +7,50 @@
 
     $db = new DBManager();
 
-    $idservice = $_POST["dominio_vehiculo"];
+  //  $idservice = $_POST["dominio_vehiculo"];
 
-    $mantenimiento = $db->altaService($service); 
+  //  $mantenimiento = $db->altaService($service); 
 ?>
 
-<form id="formEditarVehiculo">
-    <h4>Editar ficha del Vehículo:</h4>
-    <h4><?php echo $mantenimiento["DOMINIO_VEHICULO"];?></h4>
-    <input type="hidden" name="DOMINIO_VEHICULO" value="<?php echo $mantenimiento["DOMINIO_VEHICULO"]; ?>">
-    <!--<input type="hidden" name="ACTIVO" value="">-->
+<form id="">
+    <h4>Agregar nuevo mantenimiento</h4>
     <div class="row">
         <div class="input-field col s12 m6">
-            <input id="id" placeholder="Ingrese dominio a reparar" name="DOMINIO_VEHICULO" type="text" class="validate" value="<?php echo $mantenimiento["DOMINIO_VEHICULO"];?>" required disabled>
+            <input id="dominio_vehiculo" name="DOMINIO_VEHICULO" type="text" class="validate" required>
+            <label for="dominio_vehiculo">Dominio</label>
         </div>
         <div class="input-field col s12 m6">
-            <input id="fecha" placeholder="Ingrese fecha" name="FECHA" type="number" class="validate" value="<?php echo $mantenimiento["FECHA"];?>" required>
+            <input id="km_vehiculo" name="KM_VEHICULO" type="text" class="validate" required>
+            <label for="km_vehiculo">Kilometraje</label>
         </div>
     </div>
     <div class="row">
         <div class="input-field col s12 m6">
-            <input id="km_vehiculo" placeholder="Ingrese Kilometraje" name="KM_VEHICULO" type="text" class="validate" value="<?php echo $mantenimiento["KM_VEHICULO"];?>">
-        </div>                                                
+            <input name="FECHA" type="text" class="validate">
+            <label for="FECHA">Fecha</label>
+        </div>
         <div class="input-field col s12 m6">
-            <input id="costo" placeholder="Ingrese costo" name="COSTO" type="text" class="validate" value="<?php echo $mantenimiento["COSTO"];?>">
-        </div>        
-    </div>                                        
+            <input name="COSTO" type="text" class="validate">
+            <label for="COSTO">Costo</label>
+        </div>                                                
+    </div>
     <div class="row">
         <div class="input-field col s12 m6">
-            <input id="es_interno" placeholder="Ingrese intero" name="ES_INTERNO" type="number" class="validate" value="<?php echo $mantenimiento["ES_INTERNO"];?>">
-        </div>                                                
+            <input name="ES_INTERNO" type="text" class="validate">
+            <label for="ES_INTERNO">Nro.Interno</label>
+        </div>
         <div class="input-field col s12 m6">
-            <input id="COMENTARIO" placeholder="Ingrese comentario" name="COMENTARIO" type="number" class="validate" value="<?php echo $mantenimiento["COMENTARIO"];?>">
-        </div>        
-    </div>                                        
+            <input name="COMENTARIO" type="text" class="validate">
+            <label for="COMENTARIO">COMENTARIO</label>
+        </div>                                                
+    </div>                                          
     <div class="row">
         <div class="input-field col s12">
-            <a href="#!" id="btn-editar-vehiculo" class="modal-action light-blue darken-1 waves-effect waves-light btn-large">Actualizar vehículo</a>
+            <a href="#!" id="btn-nuevo-vehiculo" class="modal-action modal-close light-blue darken-1 waves-effect waves-light btn-large">Agregar Nuevo Vehiculo</a>
         </div>
-    </div>    
+    </div>
 </form>
+    
+
+
+    
